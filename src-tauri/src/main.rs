79a06accc1e16c9c65b4b266a17e69230559d2de
DavDong <372160893@qq.com>
2026-03-30 19:01:07 +0800

    feat: 初始化 Tauri 2 + Vue 3 项目脚手架

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+fn main() {
+    dclient_lib::run()
+}
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="dclient">
+    <h1>DClient</h1>
+    <p>赛博朋克终端管理器</p>
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  background: #0a0a0f;
+  color: #00ff88;
+  font-family: 'Fira Code', monospace;
+}
+</style>
